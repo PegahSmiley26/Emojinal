f522086f713d3e07758bcdd46d375c3c6e00fe68
PegahSmiley26 <PegahSmiley@gmail.com>
2019-07-11 18:31:04 -0500

    initial commit

---
+++ Emojinal/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Emojinal
+//
+//  Created by Apple on 7/11/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var heartEyes: UIButton!
+    
+    @IBOutlet weak var blackHeart: UIButton!
+    
+    @IBOutlet weak var zebra: UIButton!
+    
+    @IBOutlet weak var rainbow: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func showMessage(sender: UIButton) {
+        
+        // in as much detail as possible
+        // pseudo-code the things that need to happen
+        // when a user clicks an emoji button
+    }
+
+}
+
