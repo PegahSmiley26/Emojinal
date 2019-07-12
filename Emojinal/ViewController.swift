b082f9f02474b067195429420b3a173579ec396f
PegahSmiley26 <PegahSmiley@gmail.com>
2019-07-12 13:14:03 -0500

    done project

--- Emojinal/ViewController.swift
+++ Emojinal/ViewController.swift
@@ -10,13 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
     @IBOutlet weak var heartEyes: UIButton!
     
     @IBOutlet weak var blackHeart: UIButton!
     
-    @IBOutlet weak var zebra: UIButton!
-    
-    @IBOutlet weak var rainbow: UIButton!
+//    @IBOutlet weak var zebra: UIButton!
+//
+//    @IBOutlet weak var rainbow: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +28,36 @@ class ViewController: UIViewController {
         // in as much detail as possible
         // pseudo-code the things that need to happen
         // when a user clicks an emoji button
+        
+        var customMessages = ["heartEyes": ["You are beautiful!", "Omg look at you go!", "Wowza!!"],"blackHeart": ["One love Ari", "Thank u, next", "Break up with your gf yuh yuh cause I'm bored"]]
+        
+        let selectedEmotion = sender.titleLabel?.text
+        let emojis = ["🖤": "blackHeart" , "😍": "heartEyes"]
+        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[Int.random(in: 0...2)]
+        
+        let alertController = UIAlertController(title: "Honey,", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+        
+//        let emojiNumber = Int.random(in: 0..<4)
     }
 
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
